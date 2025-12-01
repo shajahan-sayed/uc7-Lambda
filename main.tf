c73eca0dd37a31ea495fb06799bd8d1db79990da
shajahan-sayed <shajahansayed111@gmail.com>
2025-12-01 20:00:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+#creating trust policy
+data "aws_iam_policy_document" "lambda_assume" {
+   statement {
+         effect = "Allow"
+
+
+         principals {
+              type = "services"
+              identifiers = ["lambda.amazonaws.com"]
+         }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+#creating role
+
+resource "aws_iam_role" "lambda_fun" {
+    name = "lambda_fun"
+    assume_policy = aws_iam_policy_document.lambda.assume.json
+}
+
+#creating policy and attaching it to role
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role = aws_iam_role.lambda.fun.name
+  policy = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+#creating instance profile no need because lambda directly assume the role and ec2 needs the instance profile
+
+#creating lambda function
+resource "aws_lambda_function" "lambda_func" {
+   function_name = var.function_name
+   runtime = var.lambda_runtime
+   handler = var.lambda_handler
+   role = aws_iam_role.lambda_fun.arn
+   
+   filename         = var.lambda_zip_file
+   source_code_hash = filebase64sha256(var.lambda_zip_file)
+}
+
+  
